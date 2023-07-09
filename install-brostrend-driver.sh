9ad60a45697372c918da2f889c182cf4b006c08d
Francesco Di Sales <f.disales@hotmail.com>
2023-07-10 00:47:55 +0200

    adding check to see if user is root

--- install-brostrend-driver.sh
+++ install-brostrend-driver.sh
@@ -1,2 +1,11 @@
-# to-do root control
-sh /tmp/install
+
+
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Current user is root.Trying to install the driver. Remember to put the antenna in the usb slot"
+    sh /tmp/install
+else
+    echo "Current user is not root. Installation will not boot"
+fi
+
+
+
